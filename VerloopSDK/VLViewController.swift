2ab655c059a4ee613cd6ee579cfa1d6f09a93e74
Raghav Rawat <raghav39@gmail.com>
2019-10-04 12:55:04 +0530

    adding podspec

--- VerloopSDK/VLViewController.swift
+++ VerloopSDK/VLViewController.swift
@@ -11,51 +11,47 @@ import WebKit
 
 class VLViewController: UIViewController, WKUIDelegate {
     private var verloopSDK: VerloopSDK? = nil
-    
+
     init() {
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     init(webView: VLWebViewManager) {
         super.init(nibName: nil, bundle: nil)
         setWebView(webView: webView)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
-    
 
-    
     var webView: VLWebViewManager?
-    
+
     func setWebView(webView v: VLWebViewManager) {
         webView = v
         view.addSubview(webView!.webView)
         webView!.webView.frame = view.bounds
-        
+
         if isViewLoaded {
             webView!.startRoom()
         }
     }
-    
+
     func setSDK(verloopSDK sdk: VerloopSDK) {
         verloopSDK = sdk
     }
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         if webView != nil {
             webView!.startRoom()
         }
-        
+
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "×", style: .done, target: self, action: #selector(back(_:)))
-        
+
     }
-    
+
     @objc func back(_ sender : AnyObject?) {
         if verloopSDK != nil {
             verloopSDK!.onChatClose()
